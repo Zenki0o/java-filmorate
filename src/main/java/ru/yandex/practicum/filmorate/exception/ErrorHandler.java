78a9a11ffc976460e47811de41c69338d840b912
Zenki0o <aleksei.kvasov.dev@yandex.ru>
2026-06-11 22:02:33 +0300

    add friends & likes

--- src/main/java/ru/yandex/practicum/filmorate/exception/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ErrorHandler.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.filmorate.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -27,6 +28,14 @@ public class ErrorHandler {
     public Map<String, String> handleNegativeCount(final IllegalArgumentException exception) {
         log.warn("Некорректный аргумент запроса: {}", exception.getMessage());
         return Map.of("error", "Передан отрицательный параметр count.");
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleHttpMessageNotReadableException(HttpMessageNotReadableException exception) {
+        log.warn("Некорректное тело запроса: {}", exception.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse("Тело запроса отсутствует или имеет неверный формат."));
     }
 
     @ExceptionHandler(ResponseStatusException.class)
